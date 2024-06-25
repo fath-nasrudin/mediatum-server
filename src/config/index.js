fd6a4b5c61fcbc27dd5aef658f0835b3fb1d56b6
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-25 11:42:54 +0700

    fix localhost connection string

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || '3000',
   db: {
-    stringConnection: process.env.MONGODB_URI || 'mongodb://localhost:27017'
+    stringConnection: process.env.MONGODB_URI || 'mongodb://localhost:27017/mediatum'
   }
 }
