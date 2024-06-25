16dd1619311fb4af304d403a6c05ba4684132d94
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-25 20:46:27 +0700

    Add body parser middleware

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+// body parser middleware
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
 // api
 app.use(require('./routes'));
 
