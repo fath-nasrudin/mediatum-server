1b94dc56258c56361765c759b9670fbbb41a6ff9
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-26 16:30:25 +0700

    conditioning the /articles return data based on some conditions

--- src/routes/articles.route.js
+++ src/routes/articles.route.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Article = require('../models/article.model');
+const { attachUserIfTokenExist } = require('../utils/auth')
 
 const extractProperties = (obj, properties) => {
     return properties.reduce((result, property) => {
@@ -11,40 +12,43 @@ const extractProperties = (obj, properties) => {
 };
 
 router.route('/')
-  .get(async (req, res, next) => {
-    try {
-      const {
-        page = 1,
-        limit = 0,
-        sort,
-        ...urlQueries
-      } = req.query;
-      const pageNumber = parseInt(page);
-      const limitNumber = parseInt(limit);
-      const sortString = (sort) ? sort.split(/[,;]/).join(' ') : null;
-      const filters = extractProperties(urlQueries, ['is_published']);
-      const selectString = 'title createdAt';
-      const currentPage = (!limitNumber) ? 1 : pageNumber;
+  .get([
+    attachUserIfTokenExist(),
+    async (req, res, next) => {
+      try {
+        const {
+          page = 1,
+          limit = 0,
+          sort,
+          ...urlQueries
+        } = req.query;
+        const pageNumber = parseInt(page);
+        const limitNumber = parseInt(limit);
+        const sortString = (sort) ? sort.split(/[,;]/).join(' ') : null;
+        const filters = extractProperties(urlQueries, ['is_published']);
+        const selectString = !req.user || !req.user.is_admin ? 'title createdAt' : null ;
+        const currentPage = (!limitNumber) ? 1 : pageNumber;
 
 
-      const items = await Article.find(filters)
-        .sort(sortString)
-        .skip((pageNumber - 1) * limitNumber)
-        .limit(limitNumber)
-        .select(selectString);
-      const totalItems = await Article.countDocuments();
-      const totalPages = (!limitNumber) ? 1 : Math.ceil(totalItems / limitNumber);
+        const items = await Article.find(filters)
+          .sort(sortString)
+          .skip((pageNumber - 1) * limitNumber)
+          .limit(limitNumber)
+          .select(selectString);
+        const totalItems = await Article.countDocuments();
+        const totalPages = (!limitNumber) ? 1 : Math.ceil(totalItems / limitNumber);
 
-      res.json({
-        limitNumber,
-        totalItems,
-        totalPages,
-        currentPage,
-        items,
-      });
-    } catch (error) {
-      next(error)
+        res.json({
+          limitNumber,
+          totalItems,
+          totalPages,
+          currentPage,
+          items,
+        });
+      } catch (error) {
+        next(error)
+      }
     }
-  })
+  ])
 module.exports = router;
 
