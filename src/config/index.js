c353e6b45852371dda6b440f499aeedbfe920373
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-26 07:14:24 +0700

    Add jwt config

--- src/config/index.js
+++ src/config/index.js
@@ -2,5 +2,8 @@ module.exports = {
   port: process.env.PORT || '3000',
   db: {
     stringConnection: process.env.MONGODB_URI || 'mongodb://localhost:27017/mediatum'
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
   }
 }
