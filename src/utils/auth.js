5af1e977a6e663ba758bc86ac73eb6442ba80463
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-26 16:41:12 +0700

    remove console logs

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -10,7 +10,6 @@ const attachUserIfTokenExist = () => {
     // verify valid bearer token format
     const isBearerToken = req.headers.authorization?.startsWith('Bearer ');
     if (!isBearerToken) {
-      console.log('hit failed bearer');
       return next();
     }
 
@@ -18,7 +17,6 @@ const attachUserIfTokenExist = () => {
     const [, token] = req.headers.authorization.split(' ');
     jwt.verify(token, config.jwt.secret, async (err, decoded) => {
       if (err) {
-        console.log('hit jwt verification error');
         return next();
       }
 
@@ -27,12 +25,10 @@ const attachUserIfTokenExist = () => {
       const user = await User.findById(userId);
 
       if (!user) {
-        console.log('hit jwt no user related to the id found');
         return next();
       }
 
       req.user = user;
-      console.log('should be oke');
       next();
     })
   }
