89fec9b480104967c875a37bd89fb3dc36ddc12c
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-27 16:16:54 +0700

    feat[comment]: Connect getCommentList... handler to database

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,11 +1,18 @@
-const Comment = require('./article.controller');
+const Comment = require('../models/comment.model');
 
 module.exports.getCommentListByArticleIdParam = () => [
   async (req, res, next) => {
-    res.send({
-      message: 'hit artilcleId/comments endpoint',
-      articleId: req.params.articleId,
-      id: req.params.id,
-    })
+    try {
+      let filters = { article: req.params.articleId };
+      const totalItems = await Comment.countDocuments(filters);
+      const comments = await Comment.find(filters).populate('user', 'first_name last_name username');
+
+      res.json({
+        totalItems,
+        items: comments,
+      })
+    } catch (error) {
+      next(error)
+    }
   }
 ]
