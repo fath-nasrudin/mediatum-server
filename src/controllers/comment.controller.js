f37c47306fc72abe483d4eba8170177d96c48613
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-27 18:12:23 +0700

    feat[comment]: Add pagination to getCommentList

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,13 +1,41 @@
 const Comment = require('../models/comment.model');
 
 module.exports.getCommentListByArticleIdParam = () => [
+  // pagination handler
+  (req, res, next) => {
+    const {
+          page = 1,
+          limit = 5,
+          sort,
+          ...Queries
+        } = req.query;
+      req.pagination = {
+        page: parseInt(page),
+        limit:  parseInt(limit),
+        sort,
+        Queries,
+      }
+    next();
+  },
   async (req, res, next) => {
     try {
       let filters = { article: req.params.articleId };
       const totalItems = await Comment.countDocuments(filters);
-      const comments = await Comment.find(filters).populate('user', 'first_name last_name username');
+      const comments = await Comment
+        .find(filters)
+        .sort(req.pagination.sort)
+        .skip(req.pagination.limit * (req.pagination.page - 1))
+        .limit(req.pagination.limit)
+        .populate('user', 'first_name last_name username');
+
+      const totalPages = Math.ceil(totalItems/req.pagination.limit);
+      const currentPage = req.pagination.page;
+      const limit = req.pagination.limit;
 
       res.json({
+        limit,
+        totalPages,
+        currentPage,
         totalItems,
         items: comments,
       })
