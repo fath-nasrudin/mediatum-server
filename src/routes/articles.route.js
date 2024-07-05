2fbd0c24842e75b47101fb0e7a57177137e8d713
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-07-05 19:04:31 +0700

    feat[article]: Change default sort articlelist to created_at desc

--- src/routes/articles.route.js
+++ src/routes/articles.route.js
@@ -1,42 +1,41 @@
 const router = require('express').Router();
 const Article = require('../models/article.model');
-const { attachUserIfTokenExist, checkIsAdmin, authenticate } = require('../utils/auth')
+const {
+  attachUserIfTokenExist,
+  checkIsAdmin,
+  authenticate,
+} = require('../utils/auth');
 const articleController = require('../controllers/article.controller');
 const commentController = require('../controllers/comment.controller');
- 
+
 const extractProperties = (obj, properties) => {
-    return properties.reduce((result, property) => {
-        if (obj.hasOwnProperty(property)) {
-            result[property] = obj[property];
-        }
-        return result;
-    }, {});
+  return properties.reduce((result, property) => {
+    if (obj.hasOwnProperty(property)) {
+      result[property] = obj[property];
+    }
+    return result;
+  }, {});
 };
 
-router.route('/')
+router
+  .route('/')
   .get([
     attachUserIfTokenExist(),
     async (req, res, next) => {
       try {
-        const {
-          page = 1,
-          limit = 0,
-          sort,
-          ...urlQueries
-        } = req.query;
+        const { page = 1, limit = 0, sort, ...urlQueries } = req.query;
         const pageNumber = parseInt(page);
         const limitNumber = parseInt(limit);
-        const sortString = (sort) ? sort.split(/[,;]/).join(' ') : null;
+        const sortString = sort ? sort.split(/[,;]/).join(' ') : 'created_at';
         let filters = extractProperties(urlQueries, ['is_published']);
 
         const isAdmin = req.user && req.user.is_admin;
-        let selectString =  !isAdmin ? '-is_published' : '' ;
-        selectString += ' -content'
-        const currentPage = (!limitNumber) ? 1 : pageNumber;
+        let selectString = !isAdmin ? '-is_published' : '';
+        selectString += ' -content';
+        const currentPage = !limitNumber ? 1 : pageNumber;
 
         // if the requester not an admin, hide the unpublished articles
         if (!isAdmin) filters = { ...filters, is_published: true };
-
 
         const items = await Article.find(filters)
           .sort(sortString)
@@ -45,7 +44,9 @@ router.route('/')
           .select(selectString)
           .populate('user', 'first_name last_name username');
         const totalItems = await Article.countDocuments(filters);
-        const totalPages = (!limitNumber) ? 1 : Math.ceil(totalItems / limitNumber);
+        const totalPages = !limitNumber
+          ? 1
+          : Math.ceil(totalItems / limitNumber);
 
         res.json({
           limitNumber,
@@ -55,47 +56,25 @@ router.route('/')
           items,
         });
       } catch (error) {
-        next(error)
+        next(error);
       }
-    }
+    },
   ])
-  .post([
-    authenticate(),
-    checkIsAdmin(),
-    articleController.createArticle(),
-  ])
+  .post([authenticate(), checkIsAdmin(), articleController.createArticle()]);
 
-router.route('/:articleId/comments')
-  .get([
-    commentController.getCommentListByArticleIdParam(),
-  ])
-  .post([
-    authenticate(),
-    commentController.createComment()
-  ])
+router
+  .route('/:articleId/comments')
+  .get([commentController.getCommentListByArticleIdParam()])
+  .post([authenticate(), commentController.createComment()]);
 
-router.route('/:articleId/comments/:id')
-  .put([
-    authenticate(),
-    commentController.editComment(),
-  ])
-  .delete([
-    authenticate(),
-    commentController.deleteComment()
-  ])
+router
+  .route('/:articleId/comments/:id')
+  .put([authenticate(), commentController.editComment()])
+  .delete([authenticate(), commentController.deleteComment()]);
 
-router.route('/:id')
-  .get([
-    attachUserIfTokenExist(),
-    articleController.getArticleById()
-  ])
-  .put([
-    authenticate(),
-    checkIsAdmin(),
-    articleController.updateArticle(),
-  ])
-  .delete([
-    articleController.deleteArticleByIdParam()
-  ])
+router
+  .route('/:id')
+  .get([attachUserIfTokenExist(), articleController.getArticleById()])
+  .put([authenticate(), checkIsAdmin(), articleController.updateArticle()])
+  .delete([articleController.deleteArticleByIdParam()]);
 module.exports = router;
-
